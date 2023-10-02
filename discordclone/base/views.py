9756cc2a457511595406db49317b3f1988a01cec
Emmanuel-3003 <degabriels03@gmail.com>
2023-10-02 14:37:37 +0530

    new interface

--- discordclone/base/views.py
+++ discordclone/base/views.py
@@ -96,8 +96,10 @@ def room(request, pk):
 
 def userProfile(request, pk):
     user = User.objects.get(id=pk)
-    #room = Room.objects.get(id=pk)
-    context = {'user': user}
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms': rooms, 'room_messages': room_messages, 'topics': topics}
     return render(request, 'base/profile.html', context)
 
 @login_required(login_url='login')
@@ -106,7 +108,9 @@ def createRoom(request):
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect('home')
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
